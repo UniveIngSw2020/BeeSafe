522578cbec2b3a293c18a1f6ce3beaefb229d770
Electronixxx <51324472+electronixxx@users.noreply.github.com>
2020-11-24 00:49:19 +0100

    Improved UI of App + Added language button (not functional yet)

--- app/src/main/java/com/bufferoverflow/beesafe/MainActivity.java
+++ app/src/main/java/com/bufferoverflow/beesafe/MainActivity.java
@@ -1,12 +1,23 @@
 package com.bufferoverflow.beesafe;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
+import com.bufferoverflow.beesafe.AuxTools.LocaleHelper;
 import com.bufferoverflow.beesafe.BackgroundService.BackgroundScanWork;
+import com.yarolegovich.lovelydialog.LovelyChoiceDialog;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +44,27 @@ public class MainActivity extends AppCompatActivity {
 
     public void StopService(View view) {
 
+    }
+
+    public void changeLanguage(View view) {
+        String langs[] = {"eng", "ita", "alb"};
+        List<String> l = new ArrayList<>();
+        l.add("eng");
+        l.add("ita");
+        l.add("alb");
+
+        new LovelyChoiceDialog(this)
+                .setTopColorRes(R.color.colorPrimary)
+                .setTitle("Language")
+                .setIcon(R.drawable.amu_bubble_mask)
+                .setMessage("Change language")
+                .setItems(l, new LovelyChoiceDialog.OnItemSelectedListener<String>() {
+                    @Override
+                    public void onItemSelected(int i, String s) {
+                        Toast.makeText(getApplicationContext(), s + " " + i, Toast.LENGTH_LONG).show();
+                    }
+                })
+                .show();
     }
 
 }
