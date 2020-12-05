34dfbe1be1eb6c1c901fb5fc5eb38529684ca27d
Enrico39 <enribalda1@mail.com>
2020-12-05 12:47:21 +0100

    Implements Cloud Functions Log all database lastSeen for testing :    firebase deploy --only "functions:getData"    firebase serve --only "functions:getData" by Enrico

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,33 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-admin.initializeApp();
 
+admin.initializeApp();
+admin.database();
+
+//firebase deploy --only "functions:getData"
+//firebase serve --only "functions:getData"
+// getData from database
+
+export const getData = functions.https.onRequest(async (request, response) => {
+  const db = admin.database();
+  const ref = db.ref();
+  ref.on("value", function(snapshot:any) {
+    const allDb = snapshot.val();
+    for(const k in allDb){
+      for(const i in allDb[k]){
+        console.log(allDb[k][i].lastSeen);
+      }
+    }
+    response.send(allDb);
+  }, function (error) {
+    console.log("The read failed: " + error);
+  });
+  
+})
+
+
+
+/*
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 //firebase deploy --only "functions:helloWorld"
@@ -10,6 +36,7 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 //   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+*/
 
 /*
 export const deleteOldDatas = functions.firestore
