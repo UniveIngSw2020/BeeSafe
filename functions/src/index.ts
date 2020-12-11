cbb360587ac97b33d38437b629d24b5d3c1acac6
Enrico874885 <71376123+Enrico874885@users.noreply.github.com>
2020-12-11 22:33:53 +0100

    Update index.ts
    Delete Firebase Cloud Function Cron
    Function run every 2 hours delete data older than 1 hour

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,66 +1,130 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
+//Push in Productions : firebase deploy
+//Test localhost :
+//    firebase deploy --only "functions:cron"
+//    firebase serve --only "functions:cron"
+
 admin.initializeApp();
 admin.database();
-
-//firebase deploy --only "functions:getData"
-//firebase serve --only "functions:getData"
-// getData from database
-
-export const getData = functions.https.onRequest(async (request, response) => {
+// https://firebase.google.com/docs/functions/typescript
+//Elimina dati più vecchi di un ora hours //
+export const cronData = functions.pubsub.schedule('every 2 hours ').onRun((context) => {
   const db = admin.database();
-  const ref = db.ref();
+  const ref = db.ref();//ref() take all database
+  
   ref.on("value", function(snapshot:any) {
     const allDb = snapshot.val();
-    for(const k in allDb){
-      for(const i in allDb[k]){
-        console.log(allDb[k][i].lastSeen);
+    const toDelete :any = [];
+    function elemToDelete() {
+      for(const k in allDb) {
+        for(const i in allDb[k]) {
+          try{
+            const time:String = allDb[k][i].lastSeen;//'2020-11-15T11:25:037'
+            if(time !== undefined){
+              //console.log(isValidDate(time), time);
+              if(!isValidDate(time)){
+                if(toDelete[k] === undefined){toDelete[k] = [];}
+                toDelete[k].push(i);
+              }
+            }else{
+              if(toDelete[k] === undefined){toDelete[k] = [];}
+              toDelete[k].push(i);
+              console.log('Invalid time undefined');
+            }
+          }catch(e){
+            if(toDelete[k] === undefined){toDelete[k] = [];}
+            toDelete[k].push(i);
+            console.log('Invalid time :', allDb[k][i], 'Error :', e);
+            //response.send(e);
+          }
+        }
+      }
+      return toDelete;
+    }
+    console.log(toDelete);
+    elemToDelete();
+    /* ------------------- DELETE ------------------- */
+     
+    const FieldValue = admin.firestore.FieldValue;
+    for(const k in toDelete) {
+      const dataRef = db.refFromURL('https://beesafe-bde99.firebaseio.com/'+k.toString());
+      console.log('Riferimento : '+dataRef);
+      for(const i in toDelete[k]) {
+        let info;
+        try {
+          info = dataRef.update({
+            [toDelete[k][i]]: FieldValue.delete(),
+          });
+          
+        } catch (error) {
+          console.log('Not remove obj in path : '+k+'.'+toDelete[k][i]+' Info : '+info);
+          return false;
+        }
+        
       }
     }
-    response.send(allDb);
+    /*  ---------------------------------------------------- */
+    /* ------------------- UPDATE ------------------- 
+      for(const k in toDelete) {
+        for(const i in toDelete[k]) {
+          try {
+            const dataRef = db.refFromURL('https://beesafe-bde99.firebaseio.com/'+k.toString()+'/'+toDelete[k][i]);
+            console.log('Riferimento : '+dataRef);
+            const date: Date = new Date();
+            const strDate = JSON.stringify(date);
+            const newData = strDate.substring(1, strDate.length-1);
+            const info = dataRef.update({
+              lastSeen : newData,
+            });
+            console.log(info);
+          } catch (error) {
+            console.log('Not update lastSeen obj in path : '+k+'.'+toDelete[k][i]);
+            //response.send(error);
+          }
+        }
+      }
+       ---------------------------------------------------- */
+    //response.send(toDelete);
+    return true;
   }, function (error) {
-    console.log("The read failed: " + error);
+    console.log("The read failed : " + error);
+    //response.send(error);
+    return false;
   });
   
 })
 
+function isValidDate(time : String) : Boolean {//time : '2020-11-15T11:25:037'
+  const delayHour  = 1;
+  const delay = (3600000)*(delayHour-1);//1 hour in millisec, -1 cause ita time
+  const date: Date = new Date();
+  const boundTime: Date = new Date((date.getTime() - delay));
+  
+  let strBTime = JSON.stringify(boundTime);
+  strBTime = strBTime.split('-')[2];
+  console.log("Time : " + time);
+  console.log("Bound : " + strBTime);
+  const year : number = +(time.split('-')[0]);
+  const boundY : number = boundTime.getFullYear();
+  const mouth : number = +(time.split('-')[1]);
+  const boundMou : number = boundTime.getMonth()+1;
+  const day : number = +(time.split('-')[2].substring(0, time.split('-')[2].search('T')));
+  const boundD : number = Number(strBTime.substring(0, strBTime.search('T')));
+  const hour : number = +(time.substring(time.search('T')+1, time.search(':')));
+  const boundH : number = boundTime.getHours();
+  const min : number = +(time.split(':')[1]);
+  const boundMin : number = boundTime.getMinutes();
 
+  console.log('Y['+year+' - '+boundY+'] M['+mouth+' - '+boundMou+'] D['+day+' - '+boundD+'] H['+hour+' - '+boundH+'] Min['+min+' - '+boundMin+']');
 
-/*
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-//firebase deploy --only "functions:helloWorld"
-//firebase deploy
-export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
-*/
+  const isNotValid = (year<boundY
+    || (year===boundY && mouth<boundMou)
+    || (year===boundY && mouth===boundMou && day<boundD)
+    || (year===boundY && mouth===boundMou && day===boundD && hour<boundH)
+    || (year===boundY && mouth===boundMou && day===boundD && hour===boundH && min<=boundMin));
 
-/*
-export const deleteOldDatas = functions.firestore
-  .document("beesafe-bde99")
-  .onUpdate((change, context) => {
-    // Get an object representing the document
-    const updatedPost = change.after.data() as any;
-
-    // ...or the previous value before this update
-    const oldPost = change.before.data() as any;
-
-    const oldDatas: string[] = oldPost.images;
-    const newDatas: string[] = updatedPost.images;
-
-    const deletedDatas = oldDatas.filter(oldDatas => {
-      return !newDatas.some(newDatas => newDatas === oldDatas);
-    });
-
-    const bucket = firebase.storage().bucket();
-
-    const datasRemovePromises = deletedDatas.map((dataPath: string) => {
-      return bucket.file(dataPath).delete();
-    });
-
-    return Promise.all(datasRemovePromises);
-  });
-  */
+  console.log("IsValid : "+!isNotValid)
+  return !isNotValid;
+}
